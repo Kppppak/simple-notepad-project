306797214bf611711a32affd8b45fd35aefbfb0e
ekaterinapak <pe13090@auca.kg>
2026-05-14 00:22:47 +0600

    Create base notepad Add file menu with open and save support

--- main_window.cpp
+++ main_window.cpp
@@ -1,11 +1,152 @@
 #include "main_window.h"
 
+#include <QMenuBar>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextStream>
+#include <QFile>
+
 main_window::main_window()
 {
     setWindowTitle("Notepad");
     resize(800, 600);
 
     editor = new QTextEdit(this);
-
     setCentralWidget(editor);
+
+    QMenu *file_menu =
+        menuBar()->addMenu("File");
+
+    QAction *open_action =
+        file_menu->addAction("Open");
+
+    QAction *save_action =
+        file_menu->addAction("Save");
+
+    QAction *save_as_action =
+        file_menu->addAction("Save As");
+
+    open_action->setShortcut(QKeySequence::Open);
+
+    save_action->setShortcut(QKeySequence::Save);
+
+    save_as_action->setShortcut(
+        QKeySequence::SaveAs
+    );
+
+
+    connect(
+        open_action,
+        &QAction::triggered,
+        this,
+        [this]()
+        {
+            QString file_name =
+                QFileDialog::getOpenFileName(
+                    this,
+                    "Open File"
+                );
+
+            if (file_name.isEmpty())
+            {
+                return;
+            }
+
+            QFile file(file_name);
+
+            if (!file.open(QIODevice::ReadOnly))
+            {
+                QMessageBox::critical(
+                    this,
+                    "Error",
+                    "Cannot open file"
+                );
+
+                return;
+            }
+
+            QTextStream in(&file);
+
+            editor->setText(in.readAll());
+
+            file.close();
+        }
+    );
+
+
+    connect(
+        save_action,
+        &QAction::triggered,
+        this,
+        [this]()
+        {
+            QString file_name =
+                QFileDialog::getSaveFileName(
+                    this,
+                    "Save File"
+                );
+
+            if (file_name.isEmpty())
+            {
+                return;
+            }
+
+            QFile file(file_name);
+
+            if (!file.open(QIODevice::WriteOnly))
+            {
+                QMessageBox::critical(
+                    this,
+                    "Error",
+                    "Cannot save file"
+                );
+
+                return;
+            }
+
+            QTextStream out(&file);
+
+            out << editor->toPlainText();
+
+            file.close();
+        }
+    );
+
+    connect(
+        save_as_action,
+        &QAction::triggered,
+        this,
+        [this]()
+        {
+            QString file_name =
+                QFileDialog::getSaveFileName(
+                    this,
+                    "Save As"
+                );
+
+            if (file_name.isEmpty())
+            {
+                return;
+            }
+
+            QFile file(file_name);
+
+            if (!file.open(QIODevice::WriteOnly))
+            {
+                QMessageBox::critical(
+                    this,
+                    "Error",
+                    "Cannot save file"
+                );
+
+                return;
+            }
+
+            QTextStream out(&file);
+
+            out << editor->toPlainText();
+
+            file.close();
+        }
+    );
 }
--- main_window.h
+++ main_window.h
@@ -1,15 +1,11 @@
-#ifndef MAIN_WINDOW_H
-#define MAIN_WINDOW_H
-
 #include <QMainWindow>
 #include <QTextEdit>
 
-class main_window : public QMainWindow {
+class main_window : public QMainWindow
+{
+private:
+    QTextEdit *editor;
+
 public:
     main_window();
-
-private:
-    QTextEdit* editor { nullptr };
 };
-
-#endif

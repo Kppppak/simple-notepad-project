7417a883528ea20864dfe23aea8f2e42446700cd
ekaterinapak <pe13090@auca.kg>
2026-05-14 17:55:46 +0600

    Added exception handling ti notepad

--- main_window.h
+++ main_window.h
@@ -1,10 +1,19 @@
+#pragma once
+
 #include <QMainWindow>
 #include <QTextEdit>
+#include <QString>
 
 class main_window : public QMainWindow
 {
 private:
-    QTextEdit *editor;
+    QTextEdit* editor;
+    QString current_file;
+
+    void open_file();
+    void save_file();
+    void save_file_as();
+    void update_title();
 
 public:
     main_window();
